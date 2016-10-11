d2b5bb00c0a7b615b53987cab15cd4a1bf3b9c2e
Richard <richardawale182@gmail.com>
2016-10-11 12:49:41 +0300

    first commit

---
+++ old/blog.php
@@ -0,0 +1,530 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Halpa Huolto</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/prettyPhoto.css" rel="stylesheet">
+    <link href="css/price-range.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+	<link href="css/main.css" rel="stylesheet">
+	<link href="css/responsive.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->       
+    <link rel="shortcut icon" href="images/home/favicon.png">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png">
+</head><!--/head-->
+
+<body>
+	<header id="header"><!--header-->
+	
+		
+		<div class="header-middle"><!--header-middle-->
+			<div  class="container-fluid">
+				<div  class="row">
+                                     <div style="padding-left:2%" class="col-sm-4">
+						
+                                                    <a  class="logo pull-left" href="index.php"><img class="img-responsive" src="images/home/logo5.png" alt="" /></a>
+                                                    
+						  <div   class="btn-group pull-left">    
+						<button type="button" class="btn btn-warning" data-toggle="dropdown">
+									FI
+								<i class="fa fa-angle-down"></i></button>
+								<ul class="dropdown-menu">
+									<li><a href="#">EN</a></li>
+									<li><a href="#">SW</a></li>
+								</ul>
+                                                          <button type="button" class="btn">  <a style="padding:2%;" href="login.php"><i class="fa fa-lock"></i> Login  </a> 
+                                                          </button>
+                                             </div>
+						</div>
+                                    <div class="search_box col-sm-1 ">
+							<input type="text" placeholder="Search"/>
+		
+                        </div>
+                                  
+   <div  class="col-sm-5">
+                    <div id = "top" class="navbar-header">
+                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                                    <span class="sr-only">Toggle navigation</span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                            </button>
+                    </div>
+                    <div  class="mainmenu pull-right ">
+                            <ul class="nav navbar-nav collapse navbar-collapse">
+                                    <li><a href="index.php" class="active">Home</a></li>
+                                    <li><a href="about.php">About-us</a></li>
+                                     <li><a href="networks.php">Networks</a></li>
+                                    <li><a href="blogs.php">Blog</a></li>
+                                    <li><a href="contact.php">Contact</a></li>
+
+                            </ul>
+                    </div>
+ </div>
+                    	
+					
+                                  
+                               
+					
+				</div>
+                        </div>
+                   
+                       
+                        
+                   
+		</div><!--/header-middle-->  
+	
+		
+	
+	</header><!--/header-->
+		<section id="slider"><!--slider-->
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12">
+					<div id="slider-carousel" class="carousel slide" data-ride="carousel">
+						<ol class="carousel-indicators">
+							<li data-target="#slider-carousel" data-slide-to="0" class="active"></li>
+							<li data-target="#slider-carousel" data-slide-to="1"></li>
+							<li data-target="#slider-carousel" data-slide-to="2"></li>
+						</ol>
+						
+						<div class="carousel-inner">
+							<div class="item active">
+								<div class="col-sm-6">
+									<h1><span>Cheapest</span>Summer tires Changing</h1>
+									<h2>AutoRepair Oy, Peltola - Oulu</h2>
+									<p> Cheapest offer of this summer, that comes only once</p>
+									<h1 style="margin-top:-10px;">Just on <span>10 &#8364;</span></h1>
+									<p> Grab your opportunity today</p>
+									<button type="button" class="btn btn-default get">Book your time</button>
+								</div>
+								<div class="col-sm-6">
+									<img src="images/home/tire.jpg" class="girl img-responsive" alt="" />
+									<img src="images/home/deal3.png"  class="pricing" alt="" />
+								</div>
+							</div>
+							<div class="item">
+									<div class="col-sm-6">
+									<h1><span>Free</span>Mobile Repairing</h1>
+									<h2>MobileRepair Oy, Puistokatu - Oulu</h2>
+									<p> We won't take any service charge with you !! </p>
+									<h1 style="margin-top:-10px;">Just on <span>Free &#8364;</span></h1>
+									<p> book your time within 4 pm today !!</p>
+									<button type="button" class="btn btn-default get">Book your time</button>
+								</div>
+								<div class="col-sm-6">
+									<img src="images/home/fix_phone.png" class="girl img-responsive" alt="" />
+									
+								</div>
+							</div>
+							
+							<div class="item">
+								<div class="col-sm-6">
+									<h1><span>Cheapest</span>Windows repair</h1>
+									<h2>WindowRepair Oy, Kajaaninte - Oulu</h2>
+									<p> Cheapest offer of this summer, that comes only once</p>
+									<h1 style="margin-top:-10px;">Per hour<span>20 &#8364;</span></h1>
+									<p> Grab your opportunity today</p>
+									<button type="button" class="btn btn-default get">Book your time</button>
+								</div>
+								
+								<div class="col-sm-6">
+									<img src="images/home/window.jpg" class="girl img-responsive" alt="" />
+									
+								</div>
+							</div>
+							
+						</div>
+						
+						<a href="#slider-carousel" class="left control-carousel hidden-xs" data-slide="prev">
+							<i class="fa fa-angle-left"></i>
+						</a>
+						<a href="#slider-carousel" class="right control-carousel hidden-xs" data-slide="next">
+							<i class="fa fa-angle-right"></i>
+						</a>
+					</div>
+					
+				</div>
+			</div>
+		</div>
+	</section><!--/slider-->
+	
+	<section>
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-3">
+					<div class="left-sidebar">
+						<h2>Category</h2>
+						<div class="panel-group category-products" id="accordian"><!--category-productsr-->
+							<div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title">
+										<a data-toggle="collapse" data-parent="#accordian" href="#location">
+											<span class="badge pull-right"><i class="fa fa-plus"></i></span>
+											Location
+										</a>
+									</h4>
+								</div>
+								<div id="location" class="panel-collapse collapse">
+									<div class="panel-body">
+										<ul>
+											<li><a href="#">Helsinki </a></li>
+											<li><a href="#">Oulu </a></li>
+											<li><a href="#">Vantaa </a></li>
+											<li><a href="#">Kokkola</a></li>
+											<li><a href="#"> Turku </a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+							<div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title">
+										<a data-toggle="collapse" data-parent="#accordian" href="#category">
+											<span class="badge pull-right"><i class="fa fa-plus"></i></span>
+											Category
+										</a>
+									</h4>
+								</div>
+								<div id="category" class="panel-collapse collapse">
+									<div class="panel-body">
+										<ul>
+											<li><a href="#">Home and Housing</a></li>
+											<li><a href="#">Electronics</a></li>
+											<li><a href="#">Vehicles</a></li>
+											<li><a href="#">Leasure</a></li>
+											<li><a href="#">Other</a></li>
+											<li><a href="#">Apartments and Plots</a></li>
+											
+										</ul>
+									</div>
+								</div>
+							</div>
+							
+							<div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title">
+									<h4 class="panel-title">
+										<a data-toggle="collapse" data-parent="#accordian" href="#brand">
+											<span class="badge pull-right"><i class="fa fa-plus"></i></span>
+											Brand
+										</a>
+									</h4>
+								</div>
+								<div id="brand" class="panel-collapse collapse">
+									<div class="panel-body">
+										<ul>
+											<li><a href="#">Tyota</a></li>
+											<li><a href="#">Nissan</a></li>
+											<li><a href="#">Oppel</a></li>
+											<li><a href="#">Fiat</a></li>
+											<li><a href="#">Ford</a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+								<div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title">
+										<a data-toggle="collapse" data-parent="#accordian" href="#body_type">
+											<span class="badge pull-right"><i class="fa fa-plus"></i></span>
+											Body type
+										</a>
+									</h4>
+								</div>
+								<div id="body_type" class="panel-collapse collapse">
+									<div class="panel-body">
+										<ul>
+											<li><a href="#">Farmer</a></li>
+											<li><a href="#">Sedan</a></li>
+											<li><a href="#">Jeep</a></li>
+											<li><a href="#">HatchBack</a></li>
+											<li><a href="#">Other</a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+							
+							<div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title"><a href="#">Filter Option</a></h4>
+								</div>
+							</div>
+							
+						</div><!--/category-products-->
+					
+					
+						<div class="price-range"><!--price-range-->
+							<h2>Distance</h2>
+							<div class="well text-center">
+								 <input type="text" class="span2" value="" data-slider-min="0" data-slider-max="50" data-slider-step="5" data-slider-value="[250,450]" id="sl2" ><br />
+								 <b class="pull-left">Km 0</b> <b class="pull-right">Km 50</b>
+							</div>
+						</div><!--/price-range-->
+						
+						
+					
+					</div><!--left side bar-->
+				</div>
+				<div class="col-sm-9">
+					<div class="blog-post-area">
+						<h2 class="title text-center">Latest From our Blog</h2>
+						<div class="single-blog-post">
+							<h3>Girls Pink T Shirt arrived in store</h3>
+							<div class="post-meta">
+								<ul>
+									<li><i class="fa fa-user"></i> Mac Doe</li>
+									<li><i class="fa fa-clock-o"></i> 1:33 pm</li>
+									<li><i class="fa fa-calendar"></i> DEC 5, 2013</li>
+								</ul>
+								<span>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star-half-o"></i>
+								</span>
+							</div>
+							<a href="">
+								<img src="images/blog/blog-one.jpg" alt="">
+							</a>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+							<a  class="btn btn-primary" href="">Read More</a>
+						</div>
+						<div class="single-blog-post">
+							<h3>Girls Pink T Shirt arrived in store</h3>
+							<div class="post-meta">
+								<ul>
+									<li><i class="fa fa-user"></i> Mac Doe</li>
+									<li><i class="fa fa-clock-o"></i> 1:33 pm</li>
+									<li><i class="fa fa-calendar"></i> DEC 5, 2013</li>
+								</ul>
+								<span>
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star-half-o"></i>
+								</span>
+							</div>
+							<a href="">
+								<img src="images/blog/blog-two.jpg" alt="">
+							</a>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+							<a  class="btn btn-primary" href="">Read More</a>
+						</div>
+						<div class="single-blog-post">
+							<h3>Girls Pink T Shirt arrived in store</h3>
+							<div class="post-meta">
+								<ul>
+									<li><i class="fa fa-user"></i> Mac Doe</li>
+									<li><i class="fa fa-clock-o"></i> 1:33 pm</li>
+									<li><i class="fa fa-calendar"></i> DEC 5, 2013</li>
+								</ul>
+								<span>
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star-half-o"></i>
+								</span>
+							</div>
+							<a href="">
+								<img src="images/blog/blog-three.jpg" alt="">
+							</a>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+							<a  class="btn btn-primary" href="">Read More</a>
+						</div>
+						<div class="pagination-area">
+							<ul class="pagination">
+								<li><a href="" class="active">1</a></li>
+								<li><a href="">2</a></li>
+								<li><a href="">3</a></li>
+								<li><a href=""><i class="fa fa-angle-double-right"></i></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	
+	<footer id="footer"><!--Footer-->
+		<div class="footer-top">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-2">
+						<div class="companyinfo">
+							<h2><span>e</span>-shopper</h2>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,sed do eiusmod tempor</p>
+						</div>
+					</div>
+					<div class="col-sm-7">
+						<div class="col-sm-3">
+							<div class="video-gallery text-center">
+								<a href="#">
+									<div class="iframe-img">
+										<img src="images/home/iframe1.png" alt="" />
+									</div>
+									<div class="overlay-icon">
+										<i class="fa fa-play-circle-o"></i>
+									</div>
+								</a>
+								<p>Circle of Hands</p>
+								<h2>24 DEC 2014</h2>
+							</div>
+						</div>
+						
+						<div class="col-sm-3">
+							<div class="video-gallery text-center">
+								<a href="#">
+									<div class="iframe-img">
+										<img src="images/home/iframe2.png" alt="" />
+									</div>
+									<div class="overlay-icon">
+										<i class="fa fa-play-circle-o"></i>
+									</div>
+								</a>
+								<p>Circle of Hands</p>
+								<h2>24 DEC 2014</h2>
+							</div>
+						</div>
+						
+						<div class="col-sm-3">
+							<div class="video-gallery text-center">
+								<a href="#">
+									<div class="iframe-img">
+										<img src="images/home/iframe3.png" alt="" />
+									</div>
+									<div class="overlay-icon">
+										<i class="fa fa-play-circle-o"></i>
+									</div>
+								</a>
+								<p>Circle of Hands</p>
+								<h2>24 DEC 2014</h2>
+							</div>
+						</div>
+						
+						<div class="col-sm-3">
+							<div class="video-gallery text-center">
+								<a href="#">
+									<div class="iframe-img">
+										<img src="images/home/iframe4.png" alt="" />
+									</div>
+									<div class="overlay-icon">
+										<i class="fa fa-play-circle-o"></i>
+									</div>
+								</a>
+								<p>Circle of Hands</p>
+								<h2>24 DEC 2014</h2>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<div class="address">
+							<img src="images/home/map.png" alt="" />
+							<p>505 S Atlantic Ave Virginia Beach, VA(Virginia)</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<div class="footer-widget">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-2">
+						<div class="single-widget">
+							<h2>Service</h2>
+							<ul class="nav nav-pills nav-stacked">
+								<li><a href="">Online Help</a></li>
+								<li><a href="">Contact Us</a></li>
+								<li><a href="">Order Status</a></li>
+								<li><a href="">Change Location</a></li>
+								<li><a href="">FAQ’s</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-2">
+						<div class="single-widget">
+							<h2>Quock Shop</h2>
+							<ul class="nav nav-pills nav-stacked">
+								<li><a href="">T-Shirt</a></li>
+								<li><a href="">Mens</a></li>
+								<li><a href="">Womens</a></li>
+								<li><a href="">Gift Cards</a></li>
+								<li><a href="">Shoes</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-2">
+						<div class="single-widget">
+							<h2>Policies</h2>
+							<ul class="nav nav-pills nav-stacked">
+								<li><a href="">Terms of Use</a></li>
+								<li><a href="">Privecy Policy</a></li>
+								<li><a href="">Refund Policy</a></li>
+								<li><a href="">Billing System</a></li>
+								<li><a href="">Ticket System</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-2">
+						<div class="single-widget">
+							<h2>About Shopper</h2>
+							<ul class="nav nav-pills nav-stacked">
+								<li><a href="">Company Information</a></li>
+								<li><a href="">Careers</a></li>
+								<li><a href="">Store Location</a></li>
+								<li><a href="">Affillate Program</a></li>
+								<li><a href="">Copyright</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-3 col-sm-offset-1">
+						<div class="single-widget">
+							<h2>About Shopper</h2>
+							<form action="#" class="searchform">
+								<input type="text" placeholder="Your email address" />
+								<button type="submit" class="btn btn-default"><i class="fa fa-arrow-circle-o-right"></i></button>
+								<p>Get the most recent updates from <br />our site and be updated your self...</p>
+							</form>
+						</div>
+					</div>
+					
+				</div>
+			</div>
+		</div>
+		
+		<div class="footer-bottom">
+			<div class="container">
+				<div class="row">
+					<p class="pull-left">Copyright © 2013 E-SHOPPER Inc. All rights reserved.</p>
+					<p class="pull-right">Designed by <span><a target="_blank" href="http://www.themeum.com">Themeum</a></span></p>
+				</div>
+			</div>
+		</div>
+		
+	</footer><!--/Footer-->
+	
+
+  
+    <script src="js/jquery.js"></script>
+	<script src="js/price-range.js"></script>
+	<script src="js/jquery.scrollUp.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.prettyPhoto.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>
